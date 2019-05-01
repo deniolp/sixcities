bbef752ac878680ceae1b5b35a9bf5f8f889487a
popov7 <deniolp@gmail.com>
2019-05-01 12:42:38 +0300

    Refactoring of mainPage.jsx

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,21 +1,8 @@
 import React from 'react';
 
-const stylesSvg = {
-  display: `none`
-};
-const spanSmall = {
-  width: `80%`
-};
-const spanMedium = {
-  width: `93%`
-};
-const spanBig = {
-  width: `100%`
-};
-
 const MainPage = () => {
   return <div className="page page--gray page--main">
-    <div style={stylesSvg}>
+    <div style={{display: `none`}}>
       <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path></symbol></svg>
     </div>
 
@@ -124,7 +111,7 @@ const MainPage = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={spanMedium}></span>
+                      <span style={{width: `93%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -156,7 +143,7 @@ const MainPage = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={spanSmall}></span>
+                      <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -188,7 +175,7 @@ const MainPage = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={spanSmall}></span>
+                      <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -223,7 +210,7 @@ const MainPage = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={spanBig}></span>
+                      <span style={{width: `100%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -255,7 +242,7 @@ const MainPage = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style={spanSmall}></span>
+                      <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
