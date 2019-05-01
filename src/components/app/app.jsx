6803ddac1ed64c71c41494077cdcea1cefecb78e
popov7 <deniolp@gmail.com>
2019-05-01 12:03:48 +0300

    Rendering mainPage

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import MainPage from '../main-page/main-page.jsx';
 
 const App = () => {
-  return <div>
-    <h1>Main Page</h1>
-  </div>;
+  return <MainPage
+  />;
 };
 
 export default App;
