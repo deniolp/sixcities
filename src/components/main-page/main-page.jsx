5ce34ba05a2dfaa1b0585d194e9bee36bb352bf4
popov7 <deniolp@gmail.com>
2019-05-03 13:50:44 +0300

    Creating place component and using it in main-page

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Place from '../place/place.jsx';
 
 const MainPage = (props) => {
   const {places} = props;
@@ -90,46 +91,7 @@ const MainPage = (props) => {
               </ul>
             </form>
             <div className="cities__places-list places__list tabs__content">
-              {
-                places.map((item, index) => {
-                  return <article className="cities__place-card place-card" key={index}>
-                    {
-                      item.grade ? <div className="place-card__mark">
-                        <span>{item.grade}</span>
-                      </div> : null
-                    }
-                    <div className="cities__image-wrapper place-card__image-wrapper">
-                      <a href="#">
-                        <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
-                      </a>
-                    </div>
-                    <div className="place-card__info">
-                      <div className="place-card__price-wrapper">
-                        <div className="place-card__price">
-                          <b className="place-card__price-value">&euro;{item.price}</b>
-                          <span className="place-card__price-text">&#47;&nbsp;night</span>
-                        </div>
-                        <button className={item.bookmarked ? `place-card__bookmark-button place-card__bookmark-button--active button` : `place-card__bookmark-button button`} type="button">
-                          <svg className="place-card__bookmark-icon" width="18" height="19">
-                            <use xlinkHref="#icon-bookmark"/>
-                          </svg>
-                          <span className="visually-hidden">To bookmarks</span>
-                        </button>
-                      </div>
-                      <div className="place-card__rating rating">
-                        <div className="place-card__stars rating__stars">
-                          <span style={item.ratingWidth}></span>
-                          <span className="visually-hidden">Rating</span>
-                        </div>
-                      </div>
-                      <h2 className="place-card__name">
-                        <a href="#">{item.description}</a>
-                      </h2>
-                      <p className="place-card__type">{item.type}</p>
-                    </div>
-                  </article>;
-                })
-              }
+              {places.map((item, index) => (<Place place={item} key={index} />))}
             </div>
           </section>
           <div className="cities__right-section">
