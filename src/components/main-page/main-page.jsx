4137b90392755aa34238dda74784ae5c598a168d
popov7 <deniolp@gmail.com>
2019-05-08 15:08:04 +0300

    Temporary deleting class that open ul with sorted items

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -83,7 +83,7 @@ const MainPage = (props) => {
                   <use xlinkHref="#icon-arrow-select"/>
                 </svg>
               </span>
-              <ul className="places__options places__options--custom places__options--opened">
+              <ul className="places__options places__options--custom">
                 <li className="places__option places__option--active" tabIndex="0">Popular</li>
                 <li className="places__option" tabIndex="0">Price: low to high</li>
                 <li className="places__option" tabIndex="0">Price: high to low</li>
