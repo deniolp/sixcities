a88bef300b8230fd018ed9c194b33430ce3d4bc5
popov7 <deniolp@gmail.com>
2019-05-14 22:27:33 +0300

    Adding coords to mocks

--- src/mocks/offers.js
+++ src/mocks/offers.js
@@ -7,6 +7,7 @@ export default [
     bookmarked: false,
     type: `Apartment`,
     image: `img/apartment-01.jpg`,
+    coords: [52.3909553943508, 4.85309666406198],
   },
   {
     title: `Wood and stone place`,
@@ -16,6 +17,7 @@ export default [
     bookmarked: true,
     type: `Private room`,
     image: `img/room.jpg`,
+    coords: [52.369553943508, 4.85309666406198],
   },
   {
     title: `Canal View Prinsengracht`,
@@ -25,6 +27,7 @@ export default [
     bookmarked: false,
     type: `Apartment`,
     image: `img/apartment-02.jpg`,
+    coords: [52.3909553943508, 4.929309666406198],
   },
   {
     title: `Nice, cozy, warm big bed apartment`,
@@ -34,5 +37,6 @@ export default [
     bookmarked: false,
     type: `Apartment`,
     image: `img/apartment-03.jpg`,
+    coords: [52.3809553943508, 4.939309666406198],
   },
 ];
