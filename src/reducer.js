c91d2b2b3e78af731b307c46153d2eca57fce59b
popov7 <deniolp@gmail.com>
2019-05-18 10:18:05 +0300

    Creating reducer

--- src/reducer.js
+++ src/reducer.js
@@ -63,4 +63,18 @@ const ActionCreator = {
   },
 };
 
-export {ActionCreator};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case `CHANGE_CITY`: return Object.assign({}, state, {
+      city: action.payload,
+    });
+
+    case `GET_OFFERS`: return Object.assign({}, state, {
+      offers: action.payload,
+    });
+  }
+
+  return state;
+};
+
+export {reducer, ActionCreator};
