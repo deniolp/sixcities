a9cc0cf69ac4acf2c0fe6abe2eea94d731fe9b96
popov7 <deniolp@gmail.com>
2019-05-18 10:23:44 +0300

    Adding redux with store to index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
 import App from './components/app/app';
 import offers from './mocks/offers';
 import leaflet from 'leaflet';
+import {reducer} from './reducer';
 
 const init = () => {
   const onPlaceNameClickHandler = () => {};
 
-  ReactDom.render(
-      <App
-        places={offers}
-        onClick={onPlaceNameClickHandler}
-        leaflet={leaflet}
-      />,
-      document.querySelector(`#root`)
+  const store = createStore(reducer);
+
+  ReactDom.render(<Provider store={store}>
+    <App
+      places={offers}
+      onClick={onPlaceNameClickHandler}
+      leaflet={leaflet}
+    />
+  </Provider>,
+  document.querySelector(`#root`)
   );
 };
 
