3368afbc1863ae416b9b40498aec834d822105d5
popov7 <deniolp@gmail.com>
2019-05-18 13:45:18 +0300

    Adding name of citiy to place-list's header

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -52,7 +52,7 @@ const MainPage = (props) => {
         <div className="cities__places-container container">
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{`${offers.length} ${offers.length === 1 ? `place` : `places`} to stay in Amsterdam`}</b>
+            <b className="places__found">{`${offers.length} ${offers.length === 1 ? `place` : `places`} to stay in ${city}`}</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
               <span className="places__sorting-type" tabIndex="0">
