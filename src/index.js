749636b77c057793d1831689516df2c00243f745
popov7 <deniolp@gmail.com>
2019-05-19 14:32:54 +0300

    Adding REDUX_DEVTOOLS to project

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import {reducer} from './reducer';
 const init = () => {
   const onPlaceNameClickHandler = () => {};
 
-  const store = createStore(reducer);
+  const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
   ReactDom.render(<Provider store={store}>
     <App
