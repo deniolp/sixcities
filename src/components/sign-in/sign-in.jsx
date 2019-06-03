aa4b00ab38a1854efbac9177fb7db3b31e2bcfcc
popov7 <deniolp@gmail.com>
2019-06-03 16:41:57 +0400

    Creating SignIn component

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,44 @@
+import React, {PureComponent} from 'react';
+
+class SignIn extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: ``,
+      password: ``,
+    };
+
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  render() {
+    return <main className="page__main page__main--login">
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <form className="login__form form" action="#" method="post" onSubmit={this._handleSubmit}>
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">E-mail</label>
+              <input className="login__input form__input" type="email" name="email" placeholder="Email" required=""/>
+            </div>
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">Password</label>
+              <input className="login__input form__input" type="password" name="password" placeholder="Password" required=""/>
+            </div>
+            <button className="login__submit form__submit button" type="submit">Sign in</button>
+          </form>
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <a className="locations__item-link" href="#">
+              <span>Amsterdam</span>
+            </a>
+          </div>
+        </section>
+      </div>
+    </main>;
+  }
+}
+
+export default SignIn;
