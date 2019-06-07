eed03b50359223c1f2a1033c6b7f7cb10175f7bc
popov7 <deniolp@gmail.com>
2019-06-07 13:56:45 +0400

    updating test for Favorites

--- src/components/favorites/favorites.test.js
+++ src/components/favorites/favorites.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Favorites from '../favorites/favorites';
+import {BrowserRouter} from 'react-router-dom';
 
 describe(`Favorites`, () => {
   it(`renders correctly`, () => {
-    const tree = renderer.create(<Favorites
-    />).toJSON();
+    const tree = renderer.create(<BrowserRouter>
+      <Favorites
+      />
+    </BrowserRouter>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
