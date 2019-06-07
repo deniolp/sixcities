d1705f986de35ec032ad9ab398f9885a1b9df8b1
popov7 <deniolp@gmail.com>
2019-06-07 13:51:10 +0400

    Adding Link to footer

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import {Link} from 'react-router-dom';
 
 const Favorites = () => {
   return <Fragment>
@@ -129,9 +130,9 @@ const Favorites = () => {
       </div>
     </main>
     <footer className="footer container">
-      <a className="footer__logo-link" href="#">
+      <Link to="/" className="footer__logo-link">
         <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
-      </a>
+      </Link>
     </footer>
   </Fragment>;
 };
