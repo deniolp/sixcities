93b5647766db2177e98247470200bbbf3dfc602d
popov7 <deniolp@gmail.com>
2019-06-17 14:29:27 +0400

    Writing test for AddFavoritesButton and updating others

--- src/components/place-list/place-list.test.js
+++ src/components/place-list/place-list.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
 
 import PlaceList from '../place-list/place-list';
+import {Operation} from '../../reducer/data/data';
+import {Operation as OperationUser} from '../../reducer/user/user';
+import NameSpace from '../../reducer/name-space';
+
+jest.mock(`../../reducer/data/data`);
+jest.mock(`../../reducer/user/user`);
+Operation.addToFavorites = () => (dispatch) => dispatch(jest.fn());
+Operation.deleteFromFavorites = () => (dispatch) => dispatch(jest.fn());
+OperationUser.authorizeUser = () => (dispatch) => dispatch(jest.fn());
 
 describe(`PlaceList`, () => {
   const places = [
@@ -66,11 +78,56 @@ describe(`PlaceList`, () => {
     },
   ];
 
+  const NAME_SPACE_DATA = NameSpace.DATA;
+  const NAME_SPACE_USER = NameSpace.USER;
+  const middleware = [thunk];
+  const mockStore = configureMockStore(middleware);
+  const initialState = {};
+  initialState[NAME_SPACE_DATA] = {
+    city: {},
+    offers: [],
+    reviews: [
+      {
+        id: 1,
+        comment: `Weird place`,
+        rating: 1.5,
+        date: `2019-05-16T21:02:58.227Z`,
+        user: {
+          avatarUrl: `path.jpg`,
+          id: 8,
+          isPro: false,
+          name: `Kurt`,
+        },
+      },
+      {
+        id: 2,
+        comment: `Strange place`,
+        rating: 2.5,
+        date: `2019-06-16T21:02:58.227Z`,
+        user: {
+          avatarUrl: `path.jpg`,
+          id: 9,
+          isPro: false,
+          name: `Kate`,
+        },
+      },
+    ],
+    isReviewSending: false,
+    didReviewSent: false,
+    sendError: null,
+  };
+  initialState[NAME_SPACE_USER] = {
+    user: {},
+    authError: null,
+    isAuthorizationRequired: false,
+  };
+  const store = mockStore(initialState);
+
   it(`renders correctly`, () => {
-    const tree = renderer.create(<BrowserRouter><PlaceList
+    const tree = renderer.create(<BrowserRouter><Provider store={store}><PlaceList
       offers={places}
       onClickHandler={jest.fn()}
-    /></BrowserRouter>).toJSON();
+    /></Provider></BrowserRouter>).toJSON();
 
     expect(tree).toMatchSnapshot();
   });
